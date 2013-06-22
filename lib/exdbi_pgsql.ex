aa80d3a18a65f8f9ab0f7d6bd5b616f43f347c85
Yurii Rashkovskii <yrashk@gmail.com>
2013-06-22 10:47:33 +0100

    Add DBI.PostgreSQL.connect!, better error handling, typespecs and docs

--- lib/exdbi_pgsql.ex
+++ lib/exdbi_pgsql.ex
@@ -2,6 +2,34 @@ defrecord DBI.PostgreSQL, conn: nil do
 
   alias :pgsql, as: P
 
+  @type connection
+  @type error :: DBI.PostgreSQL.Error
+
+  @typep connection_option :: {:host, String.t} |
+                              {:username, String.t} |
+                              {:password, String.t} |
+                              {:database, String.t} |
+                              {:port, non_neg_integer} |
+                              {:ssl, boolean | :required} |
+                              {:ssl_opts, [:ssl.ssl_option]} |
+                              {:timeout, timeout}
+  @type connection_options :: [connection_option]
+
+  @doc """
+  Connect to a PostgreSQL database.
+
+  Supported options:
+
+  * `host` — hostname to connect to. "localhost" by default
+  * `username` — username. Defaults to `System.get_env("USER")`
+  * `password` — password. Empty by default
+  * `database` — database name
+  * `port` — TCP port to connect to. 5432 by default
+  * `ssl` — SSL requirement, `true`, `false` or `:required`. `false` by default
+  * `ssl_options` — See `ssl` module documentation
+  * `timeout`— Timeout in milliseconds. 5000 by default.
+  """
+  @spec connect(connection_options) :: {:ok, connection} | {:ok, error}
   def connect(opts) do
      host = to_char_list(opts[:host] || "localhost")
      args = [host]
@@ -23,7 +51,31 @@ defrecord DBI.PostgreSQL, conn: nil do
                  timeout: opts[:timeout] || 5000,
                ]
      case apply(P, :connect, Enum.reverse([options|args])) do
-       {:ok, conn} -> new(conn: conn)
+       {:ok, conn} -> {:ok, new(conn: conn)}
+       {:error, :invalid_authorization_specification} ->
+         {:error,
+          DBI.PostgreSQL.Error.new severity: :error, code: "28000",
+                                   description: "Invalid authorization specification"}
+       {:error, :invalid_password} ->
+         {:error,
+          DBI.PostgreSQL.Error.new severity: :error, code: "28P01",
+                                   description: "Invalid password"}
+       {:error, error} when is_binary(error) ->
+         {:error, DBI.PostgreSQL.Error.new severity: :error, code: error,
+                                           description: "Can't connect"}
      end
+  end
+
+  @doc """
+  Connect to a PostgreSQL database. Raises an exception if unsuccessful.
+
+  For options description, read `DBI.PostgreSQL.connect/1` documentation.
+  """
+  @spec connect!(connection_options) :: connection | no_return
+  def connect!(opts) do
+    case connect(opts) do
+      {:ok, conn} -> conn
+      {:error, error} -> raise error
+    end
   end
 end
