4b40f6494b718982a5e342601393f5aebf7fe80b
Yurii Rashkovskii <yrashk@gmail.com>
2013-06-22 18:54:35 +0100

    Add expm package

---
+++ package.head.exs
@@ -0,0 +1,5 @@
+Expm.Package.new(name: "exdbi_pgsql", description: "Elixir Database Interface for PostgreSQL",
+                 version: :head, keywords: ["database","db","sql","postgres"],
+                 maintainers: [[name: "Yurii Rashkovskii",
+                                email: "yrashk@gmail.com"]],
+                 repositories: [[github: "exdbi/exdbi_pgsql"]])
